97a9e47d66f1a9c1d55c5bd6917fe919b3e253a9
Sinchan Maitri <sinchanmaitri@gmail.com>
2018-10-06 14:43:04 +0530

    First Commit

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func buttonTapped(sender:Any){
+        print("Hey, changes made")
     }
 
 
