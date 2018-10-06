09de7beff04f3b20123a6420da07d7b871e120bd
Sinchan Maitri <sinchanmaitri@gmail.com>
2018-10-06 20:00:48 +0530

    Basic Stuff

--- Podfile
+++ Podfile
@@ -1,12 +1,14 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+ platform :ios, '9.0'
 
 target 'TestGit' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
 pod 'Firebase'
+
   use_frameworks!
 
   # Pods for TestGit
+pod 'web3swift', git: 'https://github.com/BANKEX/web3swift.git'
 
   target 'TestGitTests' do
     inherit! :search_paths
--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import WebKit
+import web3swift
+
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,7 +20,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(sender:Any){
+        
+        let address = EthereumAddress("0xa5a82b196724aa24cb61e0db4e5a7ba2c3586632")!
+        let web3Main = Web3.InfuraMainnetWeb3()
+        let balanceResult = web3Main.eth.getBalance(address: address)
+        guard case .success(let balance) = balanceResult else {return}
+        
+        
+        
         print("Hey, changes made")
+        var h = String(describing: Web3.Utils.formatToEthereumUnits(balance))
+        let ethreceipt = web3Main.eth.getTransactionReceipt("0x06e2da6d0f73889d1e43a0fae3f9e283d53fa046e7528ff3be91a2fc2742d4f2")
+        print("\(web3Main.eth.getBlockByNumber(ethreceipt.value?.blockNumber ?? 0).value?.size) + \n")
+        print("\(ethreceipt)\n")
+        print(h)
+        
     }
 
 
